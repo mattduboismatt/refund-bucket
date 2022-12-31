9f39a9d8c684b6c4aa22ee1cc1a581cb5a38ff89
Matt DuBois <matt.dubois@madeintandem.com>
2022-12-31 15:07:57 -0800

    First pass at credits page + new credit form

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,4 +1,24 @@
 class CreditsController < ApplicationController
   def index
+    @credits = current_user.credits.includes(:dealer).order(created_at: :desc)
+    @dealers = Dealer.all.select(:id, :name)
+  end
+
+  def create
+    @credit = current_user.credits.new(credit_params)
+    if @credit.save
+      flash[:notice] = "Success!"
+      redirect_to credits_path
+    else
+      @dealers = Dealer.all.select(:id, :name)
+      flash[:error] = "Did not save. Please try again."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def credit_params
+    params.require(:credit).permit(:dealer_id, :amount, :notes)
   end
 end
