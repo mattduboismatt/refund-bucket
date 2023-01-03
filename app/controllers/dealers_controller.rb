4d095bced6e0040305a87257969a46126df39982
Matt DuBois <matt.dubois@madeintandem.com>
2023-01-03 10:15:23 -0800

    Add search form to dealers page

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -2,7 +2,7 @@ class DealersController < ApplicationController
   allow_unauthenticated!
 
   def index
-    flash.now[:notice] = "Welcome!"
-    @dealers = Dealer.all
+    @dealers = Dealer.search(params)
+    @dealer_categories = Dealer::CATEGORIES
   end
 end
--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -4,4 +4,16 @@ class Dealer < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
   validates :category, inclusion: {in: CATEGORIES, message: "%{value} is not a valid category"}
+
+  scope :with_name, ->(name) { where("lower(name) LIKE ?", "%#{name.downcase}%") if name.present? }
+  scope :with_category, ->(category) { where(category: category) if category.present? }
+
+  def self.search(params = {})
+    sort_column = params[:sort].presence_in(["name", "credits_count"]) || "id"
+    sort_direction = (sort_column == "credits_count") ? :desc : :asc
+
+    with_name(params[:name])
+      .with_category(params[:category])
+      .order(sort_column => sort_direction)
+  end
 end
