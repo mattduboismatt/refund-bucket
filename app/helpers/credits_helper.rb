4b29f6dd9037f5f74c61fcd914a2759ab495b965
Matt DuBois <matt.dubois@madeintandem.com>
2023-01-24 17:12:56 -0800

    Credits table headers are sort links

--- app/helpers/credits_helper.rb
+++ app/helpers/credits_helper.rb
@@ -4,4 +4,34 @@ module CreditsHelper
       render "stats", stats: stats
     end
   end
+
+  def sort_link_to(name, column)
+    name = raw("#{name} #{direction_indicator(column)}")
+
+    params = request.params.
+      merge(sort: column, direction: next_direction(column))
+
+    link_to name, params, data: {
+      turbo_action: "advance",
+      action: "turbo:click->sort-link#updateForm"
+    }
+  end
+
+  def next_direction(column)
+    if currently_sorted?(column)
+      params[:direction] == "asc" ? "desc" : "asc"
+    else
+      "asc"
+    end
+  end
+
+  def direction_indicator(column)
+    if currently_sorted?(column)
+      tag.span(class: "sort sort-#{next_direction(column)}")
+    end
+  end
+
+  def currently_sorted?(column)
+    params[:sort] == column.to_s
+  end
 end
